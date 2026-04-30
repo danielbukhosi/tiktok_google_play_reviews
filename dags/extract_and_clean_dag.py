d51fe07703aeea8fbae5110ac029785ba9bc0aa1
danielbukhosi <danieltshuma64@gmail.com>
2026-04-30 21:16:04 +0200

    decomposed the dags into separate file and modules

---
+++ dags/extract_and_clean_dag.py
@@ -0,0 +1,63 @@
+from pathlib import Path
+from datetime import datetime 
+
+from transform import remove_unnecessary_char, replace_null_values, sort_data_by_date
+
+import pandas as pd
+
+from airflow.sdk import  DAG, task
+from airflow.sdk.definitions.asset import Asset
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.providers.standard.sensors.filesystem import FileSensor
+from airflow.utils.task_group import TaskGroup
+
+BASE_DIR = Path(__file__).parent.parent # This makes sures that during runtime we do not get file not found errors, it is cwd independent for any file used.
+tiktok_google_play_reviews_filepath_csv = BASE_DIR/"data_files"/"tiktok_google_play_reviews.csv"  # PATH TO tiktok_google_play_reviews.csv
+tiktok_google_play_reviews = Asset(
+                                    name = "cleaned_tiktok_google_play_reviews",
+                                    uri = f"file://{BASE_DIR/"xcom_data_files"/"cleaned_tiktok_google_play_reviews.json"}"
+)
+
+
+with DAG(dag_id = "Extract_and_clean",schedule="@daily",start_date=datetime(2026,4,25),catchup=False) as dag:
+    wait_for_csv = FileSensor(
+                               task_id="wait_for_json_file",
+                               filepath=tiktok_google_play_reviews_filepath_csv,
+                               fs_conn_id="fs_default",
+                               poke_interval=60,
+                               timeout=60*5,
+                               mode='poke'
+
+                              )
+    @task.branch
+    def check_file_contents():
+      tiktok_google_play_reviews_csv_df = pd.read_csv(tiktok_google_play_reviews_filepath_csv)
+      df_shape = tiktok_google_play_reviews_csv_df.shape
+      rows = df_shape[0]
+      if rows != 0:
+        return "Extract_Clean.replace_null_values"
+      else:
+        return "empty_file_log"
+    
+    empty_file = BashOperator(
+                                   task_id = "empty_file_log",
+                                   bash_command= 'echo "INFO: tiktok_google_play_reviews.csv is empty: {{ds}}"'
+    )
+
+    with TaskGroup("Extract_Clean")as ExtractClean:
+      t1 = PythonOperator(
+                           task_id = "replace_null_values",
+                           python_callable=replace_null_values
+      )
+      t2 = PythonOperator(
+                           task_id = "sort_data_by_date",
+                           python_callable=sort_data_by_date
+      )
+      t3 = PythonOperator(
+                           task_id = "remove_unnecessary_char",
+                           python_callable= remove_unnecessary_char,
+                           outlets=[tiktok_google_play_reviews] # emit the asset
+      )
+      t1>>t2>>t3
+    wait_for_csv>>check_file_contents()>>[ExtractClean,empty_file]
---
+++ dags/transform.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent
+tiktok_google_play_reviews_filepath_csv = BASE_DIR/"data_files"/"tiktok_google_play_reviews.csv"
+
+
+
+def replace_null_values(**context):
+  """
+      Replace all "null" values with "-"
+  """
+
+  replace_nulls_df = pd.read_csv(tiktok_google_play_reviews_filepath_csv,header=0)
+  replace_nulls_df.fillna("-",inplace=True)
+  replace_nulls_df.to_csv(BASE_DIR/"xcom_data_files"/"removed_nulls.csv",mode='w',index=False)
+  removed_nulls_filepath = BASE_DIR/"xcom_data_files"/"removed_nulls.csv"
+  ti = context["ti"]
+  ti.xcom_push(key="removed_nulls",value =str(removed_nulls_filepath))
+
+def sort_data_by_date(**context):
+  """
+     Sort data by created_date. 
+  """
+  ti = context["ti"]
+  removed_nulls_filepath = ti.xcom_pull(task_ids="Extract_Clean.replace_null_values", key="removed_nulls")
+  sort_data_by_date_df = pd.read_csv(removed_nulls_filepath, header=0)
+  sort_data_by_date_df.rename(columns={"at":"created_at"}, inplace=True)
+  sort_data_by_date_df.sort_values(by="created_at", ascending=True, inplace=True)
+  sort_data_by_date_df.to_csv(BASE_DIR/"xcom_data_files"/"sorted_by_date.csv",mode='w',index=False)
+  sorted_data_by_date_filepath = BASE_DIR/"xcom_data_files"/"sorted_by_date.csv"
+  ti.xcom_push(key="sorted_data_by_date",value=str(sorted_data_by_date_filepath))
+
+def remove_unnecessary_char(**context):
+  """
+     Remove all unnecessary characters from the content column (e.g. smiley faces, etc.), leaving only text and punctuation marks.
+  """
+  ti = context["ti"]
+  sorted_data_by_date_filpath = ti.xcom_pull(task_ids="Extract_Clean.sort_data_by_date",key="sorted_data_by_date")
+  remove_unnecessary_char_df = pd.read_csv(sorted_data_by_date_filpath,header=0)
+  remove_unnecessary_char_df.replace(r"[^a-zA-Z0-9\s.,!?;:()\'\"-@_]","",regex=True,inplace=True)
+  remove_unnecessary_char_df.to_csv(BASE_DIR/"xcom_data_files"/"cleaned_tiktok_google_play_reviews.csv",mode='w',index=False)
+  remove_unnecessary_char_df.to_json(BASE_DIR/"xcom_data_files"/"cleaned_tiktok_google_play_reviews.json",mode="w",index=False,orient = "records")
+
+if __name__ == "__main__":
+  pass
